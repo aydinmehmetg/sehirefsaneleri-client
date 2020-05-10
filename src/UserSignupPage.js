fb9ec208303fc05241aa5c5b110697a1178d3ecd
mehmet aydın <aydinmehmet.g@gmail.com>
2020-05-10 19:31:20 +0300

    first commit

---
+++ src/UserSignupPage.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+
+class UserSignupPage extends Component {
+  //her classın Renderi override etmesi gerekir ve jsx formatı return edip dönmesi gerekiyor
+  /*İki tür componentler var bir functions ve class componentler, function componentler == stataless komponentler oluyor
+  içerisinde state(durum) taşımayan komponentlerdir. Stateful component == Class componentler ise sayfaya ait bilgileri durumları taşıyan com-
+  ponentlerdir*/
+  render() {
+    return (
+      <form>
+        <h1>Sign Up</h1>
+        <div>
+          <label>Username</label>
+          <input />
+        </div>
+        <div>
+          <label>Display Name</label>
+          <input />
+        </div>
+        <div>
+          <label>Display Name</label>
+          <input />
+        </div>
+      </form>
+    );
+  }
+}
+
+export default UserSignupPage;
